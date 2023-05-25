0e9d4aae747134c87f8dbf02d190a1cdcbb5fb0f
B3lik0 <joseg.msolis@gmail.com>
2023-05-25 13:30:51 -0600

    PWA example

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ReloadPWA />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import ReloadPWA from "./components/ReloadPWA.vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+    ReloadPWA,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
